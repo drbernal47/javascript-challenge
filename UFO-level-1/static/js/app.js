fe0e75b542f46b06c50b4b7472932497b02d48c4
Daniel Bernal <drbernal47@gmail.com>
2021-04-15 12:28:31 -0500

    Added code to pull data and fill table

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,17 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Append one table row `tr` for each ufo data object
+data.forEach((sighting) => {
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });  
+
+
+console.log(data[0]);
