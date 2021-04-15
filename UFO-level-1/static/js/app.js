8bb35162f64ae59aecda16531e50008bb31884a2
Daniel Bernal <drbernal47@gmail.com>
2021-04-15 12:49:33 -0500

    Added code to run filter button and create filtered dataset

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -14,4 +14,50 @@ data.forEach((sighting) => {
   });  
 
 
-console.log(data[0]);
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#form");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit", runEnter);
+
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+
+    // Remove previous rows of the table
+    tbody.selectAll("tr").remove();
+
+    // Filter data based on the date
+    var filteredData = data.filter(sighting => sighting['datetime'] === inputValue);
+
+    console.log(filteredData);
+
+    // Append one table row `tr` for each ufo data object
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append("td");
+        cell.text(value);
+        });
+    });  
+
+
+
+
+  };
+  
